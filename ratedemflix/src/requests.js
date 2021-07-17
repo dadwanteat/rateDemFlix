8465ca8ab4054301aa8368a2bd2fb1ef730c6eaf
Robert Dalton <dalton.robertw@gmail.com>
2021-07-17 16:36:42 -0700

    added api key fetch requests

---
+++ ratedemflix/src/requests.js
@@ -0,0 +1,14 @@
+const api_key = "19568b85f1a2a3a10883a970971c39c5"
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetlixOriginals: `/discover/tv?api_keys=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
