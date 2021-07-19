1bb2d261950ccbbb8e4a91724c3a0a2789c1859e
Robert Dalton <dalton.robertw@gmail.com>
2021-07-19 11:34:17 -0700

    morning push

--- ratedemflix/src/App.js
+++ ratedemflix/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Row from './Row'
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey you beautiful bastart, let's make rateDemFlix today</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetlixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title=
     </div>
   );
 }
--- ratedemflix/src/Row.js
+++ ratedemflix/src/Row.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import requests from './requests';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return requests;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
 
-            {/* container -> posters */}
+            <div className="">
+                {/* {several row_poster(s)} */}
 
-             
+                {movies.map()}
+            </div>
+
         </div>
     )
 }
